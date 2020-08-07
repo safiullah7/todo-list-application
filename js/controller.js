a4f0166e058547960a29e3732b088ff8806ba879
Safi Ullah <s.ullah@ssidecisions.com>
2020-08-07 10:44:57 -0700

    useless while loops, removed using trim()

--- js/controller.js
+++ js/controller.js
@@ -121,13 +121,7 @@
 	Controller.prototype.editItemSave = function (id, title) {
 		var self = this;
 
-		while (title[0] === " ") {
-			title = title.slice(1);
-		}
-
-		while (title[title.length-1] === " ") {
-			title = title.slice(0, -1);
-		}
+		title = title.trim();
 
 		if (title.length !== 0) {
 			self.model.update(id, {title: title}, function () {
