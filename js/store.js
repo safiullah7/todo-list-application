9bb9107a78305546197b1a6f2117b476e7f27d95
Safi Ullah <s.ullah@ssidecisions.com>
2020-08-07 10:41:05 -0700

    useless forloop and variable

--- js/store.js
+++ js/store.js
@@ -117,16 +117,9 @@
 	Store.prototype.remove = function (id, callback) {
 		var data = JSON.parse(localStorage[this._dbName]);
 		var todos = data.todos;
-		var todoId;
-		
-		for (var i = 0; i < todos.length; i++) {
-			if (todos[i].id == id) {
-				todoId = todos[i].id;
-			}
-		}
 
 		for (var i = 0; i < todos.length; i++) {
-			if (todos[i].id == todoId) {
+			if (todos[i].id == id) {
 				todos.splice(i, 1);
 			}
 		}
